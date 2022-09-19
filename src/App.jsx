d5523f5072df5bafe4497431707f2c8ae2deeaed
Miltonjac <miltonjac2107@gmail.com>
2022-09-19 00:22:15 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import ListaTareas from "./components/ListaTareas";
+
+function App() {
+  const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState({});
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Form 
+          tareas={tareas}
+          setTareas={setTareas}
+          tarea={tarea}
+          setTarea={setTarea}
+        />
+        <ListaTareas
+          tareas={tareas}
+          setTarea={setTarea}
+          //eliminarTarea={eliminarTarea}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
